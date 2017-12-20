ca97919387d62890eb14eefb92cfa88ce72700f8
Linda Foo <linda.foo.dev@gmail.com>
2017-12-20 11:52:42 -0800

    completed POST exercise

--- server.js
+++ server.js
@@ -35,11 +35,44 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+app.post('/shopping-list', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 });
 // As with /shopping-list, you'll want to add a couple of recipes to the model when the server starts. To add a recipe, do Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']), where the first argument is the recipe name, and the second is an array of ingredients.
+
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
 
 
 app.listen(process.env.PORT || 8080, () => {
